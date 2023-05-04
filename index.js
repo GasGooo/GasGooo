4464f3c084d4144645959ebb6637e1e0d5e86170
Lyreplus <lyreplus@gmail.com>
2023-05-04 16:02:02 +0200

    add user on db

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const db_uri = require('./secret.js');
 const express = require("express")
 
 var app = express()
@@ -11,6 +12,40 @@ app.listen(10000, function () {
 
 const { MongoClient } = require('mongodb');
 
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+const uri = db_uri; 
+
 async function main(){
-  
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    await listDatabases(client);
+
+    await createUser(client,
+            {
+                name: "Ernesto Giovannardi",
+                carplate: "SA1234FG",
+                birthdate: "11/02/1998"
+            }
+        );
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close(); 
+  } 
 }
+
+async function createUser(client, newUser){
+  const result = await client.db("myFirstDatabase").collection("userRegistered").insertOne(newUser);
+}
+
+main().catch(console.error);
