9974a212ae84e64cbe844bea5dadf274fc55b396
Lyreplus <lyreplus@gmail.com>
2023-05-11 00:23:19 +0200

    added index

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const db_uri = require('./secret.js');
 const express = require("express")
+const User = require("./model/User");
+const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-var app = express()
+const app = express();
 app.get("/",function(request,response){
   response.send("Hello World!")
 })
@@ -10,42 +13,72 @@ app.listen(10000, function () {
   console.log("Started application on port %d", 10000)
 });
 
-const { MongoClient } = require('mongodb');
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
 const uri = db_uri; 
 
+main().catch(err => console.log(err));
+
+
 async function main(){
-  const client = new MongoClient(uri);
+  await mongoose.connect(uri);
 
-  try {
-    await client.connect();
 
-    await listDatabases(client);
 
-    await createUser(client,
-            {
-                name: "Ernesto Giovannardi",
-                carplate: "SA1234FG",
-                birthdate: "11/02/1998"
-            }
-        );
+  const GiangaleazzoVisconti = new User({
+    name: 'Giangaleazzo',
+    surname: 'Visconti',
+    birthdate: new Date('2023-05-09'),
+    email: 'giangi@gmail.com',
+    password: 'passwMegaSicura',
+    address: 'Rome'
+  });
 
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close(); 
+  
+  await GiangaleazzoVisconti.save();
+  console.log(GiangaleazzoVisconti.name);
+
+//  const visconti = await User.find();
+//  console.log(visconti);
+
+
+  const readline = require('readline-sync');
+
+let choice = readline.question("Inserisci 1 se vuoi creare un account, 2 se vuoi eliminare l'account, 3 se vuoi cercare un account \n");
+
+  switch(choice) {
+    case 1:
+      let name = readline.question("Inserisci il nome \n");
+      let surname = readline.question("Inserisci il cognome \n");
+      let birthdate = readline.question("Inserisci la data di nascita in formato GG-MM-AAAA \n");
+      let email = readline.question("Inserisci la email \n");
+      let passw = readline.question("Inserisci la passw \n");
+      let address = readline.question("Dove abiti? \n");
+      let userNew = createUser(name, surname, birthdate, email, passw, address);
+      break;
+    case 2:
+      break;
+    case 3:
+    
+    default:
+      console.log("Errore");
   } 
 }
 
-async function createUser(client, newUser){
-  const result = await client.db("myFirstDatabase").collection("userRegistered").insertOne(newUser);
+async function createUser(name, surname, birthdate, mail, password, address) {
+  const newUser = new User({
+    name: name,
+    surname: surname,
+    birthdate: new Date(birthdate),
+    email: email,
+    password: passw,
+    address: address
+  });
+
+  return newUser;
 }
+
+async function findUserByEmail(email) {    
+ //TODO 
+}
+
 
 main().catch(console.error);
