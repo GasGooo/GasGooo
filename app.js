3efd33aacf924ecc42ce06ff8b18a073d2da5bbf
Lyreplus <lyreplus@gmail.com>
2023-05-17 14:57:35 +0200

    added login and middleware

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const User = require('./model/User');
 const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const auth = require("./middleware/auth");
+
+
 
 app.use(express.json());
 
@@ -61,10 +64,45 @@ app.post("/register", async (req, res) => {
 }
 });
 
+app.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+});
+
 
 // Login
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+    // Our login logic starts here
+    try {
+        // Get user input
+        const { email, password } = req.body;
 
+        // Validate user input
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+            );
+
+            // save user token
+            user.token = token;
+
+            // user
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = app;
