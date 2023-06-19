b8638bf82bc56db48c4fd98471b041ac264f37cb
Elia Zonta <elia.zontaa@gmail.com>
2023-06-19 18:51:45 +0200

    vite conversion for hot-reload

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -71,6 +71,7 @@ export default function Register() {
                   id="firstName"
                   label="First Name"
                   autoFocus
+                  
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
