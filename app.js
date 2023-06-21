71c8588ec05c481f94e837ca2af948afc299daea
Elia Zonta <elia.zontaa@gmail.com>
2023-06-21 16:20:47 +0200

    redirection fix

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.get('/favicon.ico', (req, res) => {
 });
 
 app.use(cors({
-        origin: 'http://127.0.0.1:5173',
+        origin: 'https://gasgoo.onrender.com:5173',
         optionsSuccessStatus: 200,
     })
 );
